b8c93910df9c9a45629fbab3d7baa24e05d44604
Thinker <sky456757@gmail.com>
2021-08-09 22:44:26 +0800

    msg

--- api.php
+++ api.php
@@ -29,7 +29,7 @@ if (!in_array($type, ['song','album','search','artist','playlist','lrc','url','p
 $api = new Meting($server);
 $api->format(true);
 if ($server == 'netease') {
-    $api->cookie('WM_NI=mwySEllsRW03RdXZYH6buMINpqUsUG5Oh1hSJrsMzoZCLSLI1SzEJwQVupaMTAqRvbaRdMFyBmcxoEcVWbmelHk7%2FVEgnl09HtNtxLba82JOXfZrmrbIxVDFc8wI3wN%2FMkU%3D; WM_NIKE=9ca17ae2e6ffcda170e2e6eed8d65aabe9be83c241ab9a8eb7d85a829f8eaeb666b5eea0b5d367b79de594c82af0fea7c3b92aafa9b7b4c868b0ad9eb8b33992bc98bbdc44f6bebbaecc46b4af9b94d84df791a8d2eb598aaebdaef9659a9d84b4cf43f38db6a6c97d938abb91e447989fbfb3f56aa38eacaec57382b3ad99ee4fa18a9983f06fb098a5d8ae5e87b9a28db3338cac8295fb48a2bff8d3c9459cba9693f07aa297b8b7c74ea1e8b9aab27d8ef1838cd837e2a3; WM_TID=CzeJQRKdwf1EQEQEBUd7hEKlpv%2B4x2Vj; ntes_kaola_ad=1; JSESSIONID-WYYY=FvjxnpVGYJZS7Dw%2FcytMqHlDx%2FznSwIDnwdyhZdb9y0OFQqACPXiadbTbktE8eGiXJSor%2FVG7NYNeWpZ2as66U115lG1yklmTk%5CfnqS2dgFmawp9JbOltGusloqXn8VkG4Kchtdb%5C3lZy%5C%2Fuy6KjjjoP%2FZBI4bpxxfDQt5aH0WWndH7a%3A1628518900240; _iuqxldmzr_=32; MUSIC_U=0ff5dbc42a8584a6724f88933a606bcfe82562bbf8998e103d6097dda406efbdb6834a44ed771c0ade39c620ce8469a8; __csrf=5a0de65cdecb1d872dd1db619c1517e0; __remember_me=true; playerid=46416674; csrfToken=4f2eee-cU8xDeYdOCtKzz0Tk; NMTID=00OhNf-8mvXzjF0zU3zl2qn1HbganEAAAF2QD7UuQ; _ntes_nnid=902bfc0b12dfcb88bef806810b7dc002,1607395627876; _ntes_nuid=902bfc0b12dfcb88bef806810b7dc002');
+    $api->cookie('os=pc; osver=Microsoft-Windows-10-Professional-build-10586-64bit; appver=2.9.1.199099; channel=netease; MUSIC_U=0ff5dbc42a8584a6724f88933a606bcfe82562bbf8998e103d6097dda406efbdb6834a44ed771c0ade39c620ce8469a8; __remember_me=true');
 }
 
 if ($type=='lrc') {
