8e1e8eb7f55c0f33f18d569381ed1cbd06abb502
Thinker <sky456757@gmail.com>
2021-08-09 23:18:19 +0800

    msg

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__.'/../Meting.php';
-require_once __DIR__.'/../api.php';
+require __DIR__.'/../api.php';
+require __DIR__.'/../Meting.php';
 
 
